256c33f422340cbdad185397d98f2b38f2060e82
Emilio Marcos <emilio7marcos@gmail.com>
2023-10-16 06:06:04 -0600

    navbar setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
+import { Navbar } from './Components'
 import './App.css'
 
 export default function App() {
 
   return (
     <div>
-      <h1>Yoving</h1>
+      <Navbar />
     </div>
   )
 }
---
+++ client/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import logo from '../../Assets/logo.png'
+import './Navbar.css'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img className='navbar-logo' src={logo} alt='Yoving' />
+      <a href='https://calendly.com/emiliomarcos/autonomatico-demo' target='_blank' rel='noreferrer'>
+        <button className='navbar-demo'>Agenda un Demo</button>
+      </a>
+    </nav>
+  )
+}
