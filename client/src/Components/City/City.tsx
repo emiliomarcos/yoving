b7f59dbe7c1c942a0060c012d6dddde6e4435bcb
Emilio Marcos <emilio7marcos@gmail.com>
2023-10-24 11:22:33 -0600

    city component functions

--- client/src/Components/City/City.tsx
+++ client/src/Components/City/City.tsx
@@ -1,11 +1,25 @@
+import { useState } from 'react'
 import './City.css'
 
 export default function City() {
 
+  const [city, setCity] = useState('')
+
+  function handleCity(e) {
+    setCity(e.target.value)
+  }
+
+  function handleSubmit() {
+
+  }
+
   return (
     <>
-      <form>
-        <input className='city-input' type='text' placeholder='city' />
+      <form onSubmit={handleSubmit}>
+        <input className='city-input' type='text' placeholder='City' onChange={handleCity} />
+        <br></br>
+        <br></br>
+        <button>Go</button>
       </form>
     </>
   )
