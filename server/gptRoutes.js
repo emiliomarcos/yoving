c6ed320584fdc4347d0878c7949b62f2c6b07ae1
Emilio Marcos <emilio7marcos@gmail.com>
2023-10-24 11:46:50 -0600

    fix openai api key variable

--- server/gptRoutes.js
+++ server/gptRoutes.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const router = Router();
 
-const configuration = new Configuration({apiKey: process.env.OpenAIApi});
+const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
 const openAI = new OpenAIApi(configuration);
 
 router.route('/').get(async (req, res) => res.send('GPT is awake'));
