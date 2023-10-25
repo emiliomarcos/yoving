8c709753d54f2a254ff3e288d55655ea94c789f2
Emilio Marcos <emilio7marcos@gmail.com>
2023-10-25 15:21:26 -0600

    handle event types

--- client/src/Components/City/City.tsx
+++ client/src/Components/City/City.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, ChangeEvent, FormEvent } from 'react'
 import './City.css'
 
 export default function City() {
@@ -7,12 +7,12 @@ export default function City() {
   const [gptPrompt, setGptPrompt] = useState('');
   const [experiences, setExperiences] = useState('');
 
-  function handleCity(e) {
+  function handleCity(e: ChangeEvent<HTMLInputElement>) {
     setCity(e.target.value);
     setGptPrompt(`I am someone that values nature, sunsets, cool buildings and my main hobby is yoga, I am going to ${city}`)
   }
 
-  async function handleSubmit(e) {
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
     try {
