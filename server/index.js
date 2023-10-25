eb9c4aa7eca225d87a4dd10000d4a4053b82a803
Emilio Marcos <emilio7marcos@gmail.com>
2023-10-25 14:46:40 -0600

    express json middleware

--- server/index.js
+++ server/index.js
@@ -7,10 +7,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors())
+app.use(express.json());
 app.use(cors({
   origin: ['http://127.0.0.1:5173']
-}))
+}));
 
 app.use('/gpt', gptRoutes);
 
