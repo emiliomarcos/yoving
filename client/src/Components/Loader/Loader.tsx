d0e0cbc3c7ef7d63584110adfaa195b8b7b3e393
Emilio Marcos <emilio7marcos@gmail.com>
2023-10-31 17:32:35 -0600

    create loader component

---
+++ client/src/Components/Loader/Loader.tsx
@@ -0,0 +1,9 @@
+import './Loader.css'
+
+export default function Loader() {
+  return (
+    <>
+      <h2>Loading...</h2>
+    </>
+  )
+}
--- client/src/Components/index.ts
+++ client/src/Components/index.ts
@@ -1,2 +1,3 @@
 export { default as Navbar } from './Navbar/Navbar'
 export { default as City } from './City/City'
+export { default as Loader } from './Loader/Loader'
